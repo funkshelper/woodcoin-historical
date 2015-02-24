64f3a2e5bc271bbedc2a84cc5f1c11cd5a0eeedd
josed <josed@josed-HP-630-Notebook-PC.(none)>
2015-02-24 23:32:38 +0000

    -log version tag

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-geo"
+#define CLIENT_VERSION_SUFFIX   "-log"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
