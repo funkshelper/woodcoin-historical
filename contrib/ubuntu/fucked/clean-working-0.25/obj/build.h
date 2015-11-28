0f137ba98a5f8b145f2d8416d2141cb4e40cfef2
moonpunter@gmail.com <henrypaulmadore@gmail.com>
2015-11-28 01:11:43 -0600

    drunkenly added 500mb of scrub files. sry.

--- contrib/ubuntu/fucked/clean-working-0.25/obj/build.h
+++
@@ -1,2 +0,0 @@
-// No build information available
-#define BUILD_DATE "2015-11-21 19:37:35 -0600"
--- contrib/ubuntu/fucked/oldpatches/fixobjs/obj/build.h
+++
@@ -1,2 +0,0 @@
-#define BUILD_DESC "v0.8.7.4-53-g4e6dd85-dirty"
-#define BUILD_DATE "2015-09-21 17:54:53 -0500"
